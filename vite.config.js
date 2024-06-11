94fcb09fff56999a6f2c5c8538dab9d96db7c870
Wasiq-Ali <wasiqali878@gmail.com>
2024-06-11 11:51:21 +0500

    feat: Configure PWA (WIP)

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+	VitePWA({
+		registerType: 'autoUpdate',
+		manifest: false,
+		devOptions: {
+			enabled: true,
+			type: 'module',
+		},
+	}),
   ],
   resolve: {
     alias: {
