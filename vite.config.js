76293aee4adb51e09085cc7de7678a7f856492d8
Wasiq-Ali <wasiqali878@gmail.com>
2024-07-16 13:53:29 +0500

    fix: Remove useless dependencies & remove junk code and also files..

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-import { VitePWA } from 'vite-plugin-pwa'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
     vue(),
-	VitePWA({
-		registerType: 'autoUpdate',
-		manifest: false,
-		devOptions: {
-			enabled: true,
-			type: 'module',
-		},
-	}),
   ],
   resolve: {
     alias: {
